60210d497c7e12c2c74353e4cda0759bdf6776c8
JiuMeng <1160614401@qq.com>
2018-04-30 18:59:29 +0800

    new

---
+++ share.php
@@ -0,0 +1,20 @@
+<!--share-->
+	  <ol>
+	  
+    
+    <li class="row">
+      <div class="social-share" data-initialized="true">
+	  分享：
+        <a href="#" class="social-share-icon icon-weibo"></a>
+        <a href="#" class="social-share-icon icon-qq"></a>
+        <a href="#" class="social-share-icon icon-qzone"></a>
+		<a href="#" class="social-share-icon icon-tencent"></a>
+		
+      </div>
+  
+  </ol>
+
+<script src="https://www.ohyhello.com/wp-content/themes/amativeness-master/lib/social-share.js/src/js/social-share.js"></script>
+<script>
+socialShare('#share-1');
+</script>
